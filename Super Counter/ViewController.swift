cd46edebcb701dd424a76d4e5afaddb595e01b15
Nikita Sorokin <nikitka_sorokin_04@mail.ru>
2024-03-26 23:15:51 +0300

    Добавлена функция вывода в TextView

--- Super Counter/ViewController.swift
+++ Super Counter/ViewController.swift
@@ -9,15 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
     private var value: Int = 0
-    
-    func currentTime() -> String {
-        var getDate = Date()
-        var dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
-        var formatteddate = dateFormatter.string(from: getDate)
-        return formatteddate
-    }
-    
+
+    var dateFormatter: DateFormatter = DateFormatter()
     
     @IBOutlet weak var countValueLabel: UILabel!
     @IBOutlet weak var historyValueTextView: UITextView!
@@ -29,35 +22,44 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        countValueLabel.text = "Значение счётчика: \(value)"
+        countValueLabel.text = currentValue()
         
         historyValueTextView.text = "История изменений: "
     }
     
-    
-    
-    @IBAction func increaseByOne(_ sender: Any) {
+    @IBAction private func increaseByOne(_ sender: Any) {
         value += 1
-        countValueLabel.text = "Значение счётчика: \(value)"
-        historyValueTextView.text += "\n\(currentTime()) значение изменено на +1\n"
-    }
+        countValueLabel.text = currentValue()
+        addToHistory(message: "значение увеличено на +1")
+        }
     
-    @IBAction func decreaseByOne(_ sender: Any) {
+    @IBAction private func decreaseByOne(_ sender: Any) {
         if value == 0{
-            countValueLabel.text = "Значение счётчика: \(value)"
-            historyValueTextView.text += "\n\(currentTime()) попытка уменьшить значение счётчика ниже 0\n"
+            countValueLabel.text = currentValue()
+            addToHistory(message: "попытка уменьшить значение счётчика ниже 0")
         } else {
             value -= 1
-            countValueLabel.text = "Значение счётчика: \(value)"
-            historyValueTextView.text += "\n\(currentTime()) значение изменено на -1\n"
+            countValueLabel.text = currentValue()
+            addToHistory(message: "значение уменьшено на -1")
         }
     }
     
-    @IBAction func reset(_ sender: Any) {
+    @IBAction private func reset(_ sender: Any) {
         value = 0
-        countValueLabel.text = "Значение счётчика: \(value)"
-        historyValueTextView.text += "\n\(currentTime()) значение сброшено\n"
+        countValueLabel.text = currentValue()
+        addToHistory(message: "значение сброшено")
     }
-
+    
+    private func currentTime() -> String {
+        var getDate = Date()
+        dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+        var formatteddate = dateFormatter.string(from: getDate)
+        return formatteddate
+    }
+    private func currentValue() -> String {
+        return "Значение счётчика: \(value)"
+    }
+    private func addToHistory(message: String) {
+        historyValueTextView.text += "\n\(currentTime()) \(message)\n"
+    }
 }
-
