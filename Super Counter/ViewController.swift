d6f6533a7b8b27981c51e0226ba64d956ca63f51
Nikita Sorokin <maxsuper2281337@gmail.com>
2024-03-28 15:59:45 +0300

    Замечания учтены и исправлены

--- Super Counter/ViewController.swift
+++ Super Counter/ViewController.swift
@@ -6,31 +6,45 @@
 //
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     private var value: Int = 0
-
-    var dateFormatter: DateFormatter = DateFormatter()
     
-    @IBOutlet weak var countValueLabel: UILabel!
-    @IBOutlet weak var historyValueTextView: UITextView!
-    @IBOutlet weak var increaseValueButton: UIButton!
-    @IBOutlet weak var decreaseValueButton: UIButton!
-    @IBOutlet weak var resetValueButton: UIButton!
+    private var dateFormatter: DateFormatter = DateFormatter()
+    
+    @IBOutlet weak private var countValueLabel: UILabel!
+    @IBOutlet weak private var historyValueTextView: UITextView!
+    @IBOutlet weak private var increaseValueButton: UIButton!
+    @IBOutlet weak private var decreaseValueButton: UIButton!
+    @IBOutlet weak private var resetValueButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
         countValueLabel.text = currentValue()
         
         historyValueTextView.text = "История изменений: "
     }
     
+    private func currentTime() -> String {
+        let getDate = Date()
+        dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+        let formatteddate = dateFormatter.string(from: getDate)
+        return formatteddate
+    }
+    
+    private func currentValue() -> String {
+        return "Значение счётчика: \(value)"
+    }
+    
+    private func addToHistory(message: String) {
+        historyValueTextView.text += "\n\(currentTime()): \(message)\n"
+    }
+    
     @IBAction private func increaseByOne(_ sender: Any) {
         value += 1
         countValueLabel.text = currentValue()
         addToHistory(message: "значение увеличено на +1")
-        }
+    }
     
     @IBAction private func decreaseByOne(_ sender: Any) {
         if value == 0{
@@ -47,18 +61,5 @@ class ViewController: UIViewController {
         value = 0
         countValueLabel.text = currentValue()
         addToHistory(message: "значение сброшено")
-    }
-    
-    private func currentTime() -> String {
-        let getDate = Date()
-        dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
-        let formatteddate = dateFormatter.string(from: getDate)
-        return formatteddate
-    }
-    private func currentValue() -> String {
-        return "Значение счётчика: \(value)"
-    }
-    private func addToHistory(message: String) {
-        historyValueTextView.text += "\n\(currentTime()): \(message)\n"
     }
 }
